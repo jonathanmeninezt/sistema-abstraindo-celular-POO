58767c024cb4ebb049fdac2e8acecfafe2a97b70
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-09-20 12:31:51 -0300

    Desafio smartphone concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,20 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        //  Implementado!!!   **TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get; set; }
+        protected string Modelo { get; set; }
+        protected string Imei { get; set; }
+        protected int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            // Implementado!!!  **TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+            Console.WriteLine("Celular Nokia: ");
+            Nokia nokia = new Nokia("123456", "Nokia 3310", "IMEI123", 16);
+            nokia.Ligar();
+            // Instalar um aplicativo no Nokia
+            nokia.InstalarAplicativo("Banco Pan");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Celular Iphone: ");
+            Iphone iphone = new Iphone("1458", "AP-15", "IMEI256", 264);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
